717c328679636b81d5a7fb7dacc8af94b81d115a
Erika Cannon <cannone.rva@gmail.com>
2022-03-23 16:19:45 -0400

    start button triggers startGame function

--- develop/script.js
+++ develop/script.js
@@ -25,10 +25,41 @@ var questionLog = 0;
 var playerScore = 0;
 var timer, counter = 0;
 
+//show multiple choice function
 function showQuestion() {
-    getElementById("question").text(questions[questionLog].question)
-    getElementById("answer-1").text(questions[questionLog].answers[0])
-    getElementById("answer-2").text(questions[questionLog].answers[1])
-    getElementById("answer-3").text(questions[questionLog].answers[2])
-    getElementById("answer-4").text(questions[questionLog].answers[3])
+    document.getElementById("question").innerHTML = (questions[questionLog].question);
+    document.getElementById("answer-1").innerHTML = (questions[questionLog].answers[0]);
+    document.getElementById("answer-2").innerHTML = (questions[questionLog].answers[1]);
+    document.getElementById("answer-3").innerHTML = (questions[questionLog].answers[2]);
+    document.getElementById("answer-4").innerHTML = (questions[questionLog].answers[3]);
+};
+
+//timer countdown function
+function countdown() {
+   document.getElementById("counter").innerHTML = (counter);
+    timer = setInterval(function() {
+        counter--;
+        getElementById("counter").text(counter);
+        if (counter === 0) {
+            clearInterval(timer);
+            endGame();
+        }
+    }, 1000);
 }
+
+//start game function
+function startGame() {
+    var quizSlide= document.getElementById("quizContent");
+    var quizIntructions = document.getElementById("introSlide");
+    quizSlide.classList.remove("invisible");
+    quizIntructions.classList.add("invisible");
+    showQuestion();
+
+}
+
+//start end game function-- presents form to enter intitials 
+function endGame() {
+
+}
+
+document.getElementById("startButton").addEventListener("click", startGame);
